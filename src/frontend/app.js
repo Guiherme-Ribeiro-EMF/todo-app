2c4914a7ac9b7d5ff4df48985e6db1ac8f9a7319
Guilherme-Ribeiro-emf <143508598+Guiherme-Ribeiro-EMF@users.noreply.github.com>
2025-05-03 15:50:34 +0200

    Update app.js

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "https://baqtestappbackend.blackglacier-c7b0844e.northeurope.azurecontainerapps.io/api/tasks";
+const apiEndpoint = "https://todo-app-300722-chy-210-gui.salmonmushroom-069affea.northeurope.azurecontainerapps.io/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
