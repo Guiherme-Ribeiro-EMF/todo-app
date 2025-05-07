ccc4ec3fa8e1d06eeb5fd3f5ae3f6e511b30e1de
Guilherme-Ribeiro-emf <143508598+Guiherme-Ribeiro-EMF@users.noreply.github.com>
2025-05-07 09:12:44 +0200

    Update .lighthouserc.js

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -6,8 +6,8 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'categories:performance': ['error', { minScore: 0.8 }],
-        'categories:accessibility': ['error', { minScore: 0.9 }]
+        'categories:performance': ['error', { minScore: 0.5 }],
+        'categories:accessibility': ['error', { minScore: 0.5 }]
       }
     },
     upload: {
